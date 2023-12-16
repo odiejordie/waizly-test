fc9434dd228450bbbb0d639ba8774a3a76fbc81f
Bambang Skidipap <odiejordie@gmail.com>
2023-12-16 23:46:51 +0700

    adjust tampilan login

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,15 +1,32 @@
-export default function PrimaryButton({ className = '', disabled, children, ...props }) {
-    return (
-        <button
-            {...props}
-            className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+export default function PrimaryButton({
+  className = "",
+  disabled,
+  children,
+  ...props
+}) {
+  return (
+    // <button
+    //   {...props}
+    //   className={
+    //     `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
+    //       disabled && "opacity-25"
+    //     } ` + className
+    //   }
+    //   disabled={disabled}
+    // >
+    //   {children}
+    // </button>
+    <div className={`group/btn-login relative ` + className}>
+      <button
+        {...props}
+        disabled={disabled}
+        className="font-silkscreen relative z-10 w-full rounded-md bg-gray-800 p-1.5 text-xs text-white outline-gray-500 transition-all hover:bg-gray-500 group-hover/btn-login:-translate-x-1 group-hover/btn-login:-translate-y-1"
+      >
+        {disabled ? "Tunggu Man" : children}
+      </button>
+      <div className="font-silkscreen absolute top-0 z-0 w-full rounded-md border border-gray-500 bg-transparent p-1 text-xs text-transparent transition-all group-hover/btn-login:translate-x-0.5 group-hover/btn-login:translate-y-0.5">
+        {children}
+      </div>
+    </div>
+  );
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -8,35 +8,33 @@ import TextInput from "@/Components/TextInput";
 import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        email: "",
-        password: "",
-        remember: false,
-    });
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: "",
+    password: "",
+    remember: false,
+  });
 
-    useEffect(() => {
-        return () => {
-            reset("password");
-        };
-    }, []);
-
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route("login"));
+  useEffect(() => {
+    return () => {
+      reset("password");
     };
+  }, []);
 
-    return (
-        <GuestLayout>
-            <Head title="Masuk Bang" />
+  const submit = (e) => {
+    e.preventDefault();
 
-            {status && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    {status}
-                </div>
-            )}
+    post(route("login"));
+  };
 
-            {/* <form onSubmit={submit}>
+  return (
+    <GuestLayout>
+      <Head title="Masuk Bang" />
+
+      {status && (
+        <div className="mb-4 font-medium text-sm text-green-600">{status}</div>
+      )}
+
+      {/* <form onSubmit={submit}>
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
@@ -96,73 +94,70 @@ export default function Login({ status, canResetPassword }) {
                     </PrimaryButton>
                 </div>
             </form> */}
-            <div className="flex h-full w-full flex-col justify-between bg-gradient-to-tr from-gray-200 to-gray-100 p-2 shadow-2xl md:h-1/2 md:w-1/2 md:flex-row md:rounded-2xl md:bg-right">
-                <div className="p-5 md:w-1/2"></div>
-                <div className="flex h-1/2 flex-col justify-between rounded-xl bg-white p-5 drop-shadow md:h-full md:w-2/3">
-                    <div className="font-silkscreen cursor-default text-right text-2xl uppercase text-gray-800 md:text-left transition-color hover:text-gray-600">
-                        <p>Meminjam</p>
-                        <p>Dengan</p>
-                        <p>Gaya</p>
-                    </div>
-                    <form
-                        onSubmit={submit}
-                        className="flex w-full flex-col gap-2"
-                    >
-                        <div className="flex flex-col gap-1">
-                            <input
-                                type="text"
-                                placeholder="Email"
-                                name="email"
-                                autoComplete="username"
-                                value={data.email}
-                                onChange={(e) =>
-                                    setData("email", e.target.value)
-                                }
-                                className="rounded-md text-gray-800 border p-1.5 px-2 text-sm outline-gray-500 placeholder:text-xs"
-                            />
+      <div className="flex h-full w-full flex-col justify-between bg-gradient-to-tr from-gray-200 to-gray-100 p-2 shadow-2xl md:h-1/2 md:w-1/2 md:flex-row md:rounded-2xl md:bg-right">
+        <div className="p-5 md:w-1/2"></div>
+        <div className="flex h-1/2 flex-col justify-between rounded-xl bg-white p-5 drop-shadow md:h-full md:w-1/2">
+          <div className="font-silkscreen cursor-default text-right text-2xl uppercase text-gray-800 md:text-left transition-color hover:text-gray-600">
+            <p>Meminjam</p>
+            <p>Dengan</p>
+            <p>Gaya</p>
+          </div>
+          <form onSubmit={submit} className="flex w-full flex-col gap-2">
+            <h1 className="text-lg font-silkscreen">Masuk...</h1>
+            <div className="flex flex-col gap-1 relative">
+              <TextInput
+                type="text"
+                id="email"
+                placeholder="Email"
+                name="email"
+                autoComplete="username"
+                value={data.email}
+                onChange={(e) => setData("email", e.target.value)}
+                className="peer placeholder-transparent"
+              />
 
-                            <InputError
-                                message={errors.email}
-                                className="text-xs"
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                name="password"
-                                autoComplete="current-password"
-                                onChange={(e) =>
-                                    setData("password", e.target.value)
-                                }
-                                className="rounded-md text-gray-800 border p-1.5 px-2 text-sm outline-gray-500 placeholder:text-xs"
-                            />
-                            <InputError
-                                message={errors.password}
-                                className="text-xs"
-                            />
-                        </div>
-                        <div className="group/btn-login relative mt-1.5 w-full">
-                            <button
-                                type="submit"
-                                disabled={processing}
-                                className="font-silkscreen relative z-10 w-full rounded-md bg-gray-800 p-1.5 text-xs text-white outline-gray-500 transition-all hover:bg-gray-500 group-hover/btn-login:-translate-x-1 group-hover/btn-login:-translate-y-1"
-                            >
-                                {processing ? "Tunggu Man" : "Masuk"}
-                            </button>
-                            <div className="font-silkscreen absolute top-0 z-0 w-full rounded-md border border-gray-500 bg-transparent p-1 text-xs text-transparent transition-all group-hover/btn-login:translate-x-0.5 group-hover/btn-login:translate-y-0.5">
-                                Masuk
-                            </div>
-                        </div>
-                        <p className="mt-5 cursor-default text-right text-xs text-gray-800">
-                            Belum punya akun?{" "}
-                            <span className="cursor-pointer font-bold hover:underline transition-color hover:text-gray-600">
-                                Daftar Sekarang
-                            </span>
-                        </p>
-                    </form>
-                </div>
+              <InputLabel
+                for="email"
+                className="absolute text-xs left-2 border border-transparent -top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-gray-800 peer-placeholder-shown:top-1.5"
+              >
+                Email
+              </InputLabel>
+
+              <InputError message={errors.email} className="text-xs" />
             </div>
-        </GuestLayout>
-    );
+            <div className="flex flex-col gap-1 relative">
+              <TextInput
+                type="password"
+                id="password"
+                placeholder="Password"
+                name="password"
+                autoComplete="current-password"
+                value={data.password}
+                onChange={(e) => setData("password", e.target.value)}
+                className="peer placeholder-transparent"
+              />
+
+              <InputLabel
+                for="email"
+                className="absolute text-xs left-2 border border-transparent -top-2 peer-placeholder-shown:text-sm peer-placeholder-shown:bg-transparent peer-placeholder-shown:text-gray-800 peer-placeholder-shown:top-1.5"
+              >
+                Password
+              </InputLabel>
+
+              <InputError message={errors.password} className="text-xs" />
+            </div>
+            <PrimaryButton className="mt-1.5 w-full" disabled={processing}>
+              Masuk
+            </PrimaryButton>
+            <p className="mt-5 cursor-default text-right text-xs text-gray-800">
+              Belum punya akun?{" "}
+              <span className="cursor-pointer font-bold hover:underline transition-color hover:text-gray-600">
+                Daftar Sekarang
+              </span>
+            </p>
+          </form>
+        </div>
+      </div>
+    </GuestLayout>
+  );
 }
